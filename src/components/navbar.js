d555ecf821b9ec54bc357428d4c51ddb63a9da18
B-UDAY-CHANDRA-001 <budaychandra2014@gmail.com>
2021-11-17 21:32:24 +0530

    home page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../images/nav_logo.png";
+import logo from "../images/final_logo.png";
 // import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 // import { CgGitFork } from "react-icons/cg";
@@ -38,6 +38,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/portfolio">
+          {/* <img src={logo1} className="img-fluid logo" alt="brand" /> */}
           <img src={logo} className="img-fluid logo" alt="brand" />
         </Navbar.Brand>
         <Navbar.Toggle
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,112 @@
 // import { width } from 'dom-helpers';
-import React from 'react';
-import BackgroundVideo from "../videos/background_video.mp4"
+// import React from 'react';
+import BackgroundVideo from "../videos/black.mp4";
+import Home2 from "./home2";
+import test from "../images/test1.png"
+import React, { useEffect, useState } from "react";
+const styles = {
+  web_heading: {
+        left:'10em',
+        top:'2em',
+        
+  },
+}
+
+
+
 const Home = () => {
+
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 767);
+const updateMedia = () => {
+  setDesktop(window.innerWidth > 767);
+};
+
+useEffect(() => {
+  window.addEventListener("resize", updateMedia);
+  return () => window.removeEventListener("resize", updateMedia);
+});
+
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <video
-        autoPlay loop muted
-        style={{
-          position: "absolute",
-          width: "100%",
-          left : "50%",
-          top : "50%",
-          height : "100%",
-          objectFit : "cover",
-          transform : "translate(-50%, -50%)",
-          zIndex: "-1"
+    <div >
+      {isDesktop ? (
+        <div>
+          <video
+            autoPlay loop muted
+            style={{
+              position: "absolute",
+              width: "100%",
+              left : "50%",
+              top : "50%",
+              height : "100%",
+              objectFit : "cover",
+              transform : "translate(-50%, -50%)",
+              zIndex: "-1"
 
 
-        }}
-      >
-        <source src = {BackgroundVideo} type = "video/mp4"/> 
-      </video>
-      <h1>Home</h1>
+            }}
+          
+            >
+            <source src = {BackgroundVideo} type = "video/mp4"/> 
+          </video>
+          
+            
+          
+          <div  style={{ display:'flex'}}>
+            <img  className="myAvtar" src = {test} alt="avtar" />
+            <h1  className="home-content"><span > Hi There ..! </span><br/><br/> I'M 
+            <br/><strong class="purple">UDAY CHANDRA BHOOKYA</strong>
+            <br/><strong>Computer Science Engineer</strong>
+            </h1>
+            
+           
+          <Home2/>
+            
+          </div>
+          
+          
+          
+        
+        
+          
+        </div>
+      ) : //mobie view-----------------------------------------------------------
+      (
+        <div>
+          <video
+            autoPlay loop muted
+            style={{
+              position: "absolute",
+              width: "100%",
+              left : "50%",
+              top : "50%",
+              height : "100%",
+              objectFit : "cover",
+              transform : "translate(-50%, -50%)",
+              zIndex: "-1"
+
+
+            }}
+          
+            >
+            <source src = {BackgroundVideo} type = "video/mp4"/> 
+          </video>
+          
+          <div  style={{ display:'flex' , flexDirection: 'column'}}>
+            <img  className="myAvtar_mobile" src = {test} alt="avtar" />
+            <h1  className="home-content-mobile"><span class="purple">  Hi There ..! </span><br/><br/> I'M <br/>
+            <strong class="purple">UDAY CHANDRA B</strong>
+            <br/><br/><strong>Computer Science Engineer</strong></h1>
+            
+          </div>
+          
+
+          <Home2/>
+
+        </div>
+      )}
     </div>
+
   );
 };
 
