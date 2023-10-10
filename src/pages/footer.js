59503dab74ad90116a96d2b163981fa58f678e9c
B-UDAY-CHANDRA-001 <budaychandra2014@gmail.com>
2023-10-10 21:49:31 +0530

    Instagram link update

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -45,7 +45,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/uday.chandra_/"
+                href="https://www.instagram.com/_udaychandra/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
